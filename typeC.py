34f027c3817651618e9f437c09e9da5e9f7e7c29
Parisha <parishameerut@gmail.com>
2022-06-22 20:42:20 +0530

    re

--- typeC.py
+++ typeC.py
@@ -21,16 +21,19 @@ opcode_dict = {"add":"10000",
                 "je":"01111",
                 "hlt":"01010"}
 
-reg_dict ={'R0':'000' ,
-            'R1':'001' , 
-            'R2':'010' , 
-            'R3':'011' , 
-            'R4':'100' , 
-            'R5':'101' , 
-            'R6':'110' , 
+reg_dict ={'reg0':'000' ,
+            'reg1':'001' , 
+            'reg2':'010' , 
+            'reg3':'011' , 
+            'reg4':'100' , 
+            'reg5':'101' , 
+            'reg6':'110' , 
             'FLAGS':'111'}
 
 ins = 'mov reg1 reg2'
+#ins = 'div reg1 reg2'
+#ins = 'not reg1 reg2'
+#ins = 'cmp reg1 reg2'
 
 machine_code = []
 
@@ -51,9 +54,6 @@ machine_code.append(reg1)
 reg2 = reg_dict.get(ins_list[2])
 machine_code.append(reg2)
 
-print(ins_list)
 
-print(machine_code)
-
-#ans = ''.join(machine_code)
-#print(ans)
+ans = ''.join(machine_code)
+print(ans)
