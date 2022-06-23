e56a417a2c21cebd8cb3188397d67d53b6f0f588
Parisha <parishameerut@gmail.com>
2022-06-23 15:40:28 +0530

    typec

--- typeC.py
+++ typeC.py
@@ -1,59 +1,15 @@
-# mov(reg to reg), div, not, cmp
+def typec(opcode, register_dict, list_instr):
+    for keys in opcode.keys():
+        if str(list_instr[0]).lower() == str(keys).lower():
+            op = opcode[keys][0]
 
-opcode_dict = {"add":"10000",
-                "sub":"10001",
-                "movi":"10010",
-                "mov":"10011",
-                "ld":"10100",
-                "st":"10101",
-                "mul":"10110",
-                "div":"10111",
-                "rs":"11000",
-                "ls":"11001",
-                "xor":"11010",
-                "or":"11011",
-                "and":"11100",
-                "not":"11101",
-                "cmp":"11110",
-                "jmp":"11111",
-                "jlt":"01100",
-                "jgt":"01101",
-                "je":"01111",
-                "hlt":"01010"}
+    for reg in register_dict.keys():
+        if str(list_instr[1]).lower() == str(reg).lower():
+            r1 = register_dict[reg]
 
-reg_dict ={'reg0':'000' ,
-            'reg1':'001' , 
-            'reg2':'010' , 
-            'reg3':'011' , 
-            'reg4':'100' , 
-            'reg5':'101' , 
-            'reg6':'110' , 
-            'FLAGS':'111'}
+    for reg in register_dict.keys():
+        if str(list_instr[2]).lower() == str(reg).lower():
+            r2 = register_dict[reg]
 
-ins = 'mov reg1 reg2'
-#ins = 'div reg1 reg2'
-#ins = 'not reg1 reg2'
-#ins = 'cmp reg1 reg2'
-
-machine_code = []
-
-ins_list = ins.split()
-
-# if $ in ins:
-#    type B
-
-op = opcode_dict.get(ins_list[0])
-machine_code.append(op)
-
-machine_code.append('00000')
-
-
-reg1 = reg_dict.get(ins_list[1])
-machine_code.append(reg1)
-
-reg2 = reg_dict.get(ins_list[2])
-machine_code.append(reg2)
-
-
-ans = ''.join(machine_code)
-print(ans)
+    mc = op + '00000' + r1 + r2
+    print(mc)
