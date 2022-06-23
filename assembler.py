bbd3884bf9a80be15b3d9856996620f73db69007
vartika2401 <vartika3004@gmail.com>
2022-06-23 15:19:41 +0530

    final

--- assembler.py
+++ assembler.py
@@ -1,18 +1,3 @@
-# from multiprocessing.sharedctypes import Value
-
-
-# def Addition(reg1,reg2,reg3):
-#     reg3 = reg2+reg1
-# def Subtraction(reg1,reg2,reg3):
-#     reg3 = reg1-reg2
-# def MoveImmediate(reg1,value):
-#     reg1 = value
-# def MoveRegister(reg1,reg2):
-#     reg2 = reg1
-# def Load(reg1,mem):
-
-# def Store()
-#opcodes 
 opcode = {"add":["10000","a"],
         "sub":["10001","a"],
         "movi":["10010","b"],
@@ -35,36 +20,43 @@ opcode = {"add":["10000","a"],
         "hlt":["01010","f"]
 
         }
-register_dict = {'reg0':'000' ,
-                 'reg1':'001' , 
-                 'reg2':'010' , 
-                 'reg3':'011' , 
-                 'reg4':'100' , 
-                 'reg5':'101' , 
-                 'reg6':'110' , 
+register_dict = {'R0':'000' ,
+                 'R1':'001' ,
+                 'R2':'010' ,
+                 'R3':'011' ,
+                 'R4':'100' ,
+                 'R5':'101' ,
+                 'R6':'110' ,
                  'FLAGS':'111'}
-# def a():
+
+def a(list_instr):
+    print(opcode[list_instr[0]][0] + "00" + register_dict[list_instr[1]] + register_dict[list_instr[2]] + register_dict[list_instr[3]])
 
 
-# opcode = {"10000":Addition,
-#         "10001":Subtraction,
-#         "10010":MoveImmediate,
-#         "10011":MoveRegister,
-#         "10100":Load,
-#         "10101":Store,
-#         "10110":Multiply,
-#         "10111":Divide,
-#         "11000":RightShift,
-#         "11001":LeftShift,
-#         "11010":ExclusiveOR,
-#         "11011":Or,
-#         "11100":And,
-#         "11101":Invert,
-#         "11101":Compare,
-#         "11111":uncond_jump,
-#         "01100":ljump,
-#         "01101":gjump,
-#         "01111":ejump,
-#         "01010":Halt
-#         }
+def typeb(opcode, register_dict, list_instr):
+    for keys in opcode.keys():
+        if str(list_instr[0]).lower() == str(keys).lower():
+            op = opcode[keys][0]
+
+    for reg in register_dict.keys():
+        if str(list_instr[1]).lower() == str(reg).lower():
+            r = register_dict[reg]
+    b = str(bin(int(list_instr[2][1:])))[2:]
+    while len(b) != 8:
+        b = '0' + b
+    mc = op + r + b
+    print(mc)
+
+file = open("tezt.txt","r")
+for line in file:
+    list_instr = []
+    for i in line.split():
+        list_instr.append(i)
+    if opcode[list_instr[0]][1]=="a":
+        a(list_instr)
+    elif list_instr[0]=="mov" and list_instr[2][:1]=="$":
+        typeb(opcode,register_dict,list_instr)
+    elif opcode[list_instr[0]][1]=="b":
+
+        typeb(opcode,register_dict,list_instr)
 
