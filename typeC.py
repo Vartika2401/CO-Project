d578827efd06e24efa1f96cead7545c2ecf34680
zubaida03 <108005228+zubaida03@users.noreply.github.com>
2022-06-24 20:07:49 +0530

    Update typeC.py

--- typeC.py
+++ typeC.py
@@ -1,4 +1,7 @@
 def typeC(opcode, register_dict, list_instr):
+    if list_instr[1]=='FLAGS':
+        print(f'ERROR')
+        return
     for keys in opcode.keys():
         if str(list_instr[0]).lower() == str(keys).lower():
             op = opcode[keys][0]
