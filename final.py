9fe1cd81e591d205a7450a3a00cfe4d68b7b697e
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2022-06-28 15:00:07 +0530

    Update final.py

--- final.py
+++ final.py
@@ -100,18 +100,25 @@ output = open("answer.txt","a")
 count = 0
 var_count = 0
 var = []
+ans = 0
+labels={}
 for line in file:
     list_instr = []
     for i in line.split():
         list_instr.append(i)
-
     if list_instr != [] :
         if list_instr[0] == 'var':
-            var.append(list_instr[1])
-            var_count += 1
+            if list_instr[1] not in opcode.keys() and list_instr[1] not in register_dict.keys():
+                var.append(list_instr[1])
+                var_count += 1
+            else:
+                ans = "error"
+                output.write(ans)
         else:
             count += 1
             random.append(list_instr)
+        if list_instr[0][-1:]==":":
+            labels[list_instr[0][0:-1]]=count-1
 for i in range(count):
     if opcode[random[i][0]][1] == "a":
         a(random[i])
@@ -137,3 +144,4 @@ for i in range(count):
             break
     elif random[i][0] == "hlt":
         print(opcode[random[i][0]][0] + "00000000000")
+
