a9e2ada8726efa3604d4e2aefaff92e136ee9ad0
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2022-06-28 15:28:36 +0530

    Update typee.py

--- typee.py
+++ typee.py
@@ -5,9 +5,5 @@ def typee(opcode, register_dict, list_instr):
     for keys in opcode.keys():
         if str(list_instr[0]).lower() == str(keys).lower():
             op = opcode[keys][0]
-
-    mem_add = str(bin(var.index(list_instr[1])+count))[2:]
-    while len(mem_add)!=8:
-        mem_add="0"+mem_add
-    print(op+"000"+mem_add)
+    ans = op+"00000000000"
 
