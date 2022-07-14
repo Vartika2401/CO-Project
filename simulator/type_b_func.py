d0e492671f10a54b90f04ae7158f1d188899a250
Parisha <parishameerut@gmail.com>
2022-07-14 20:13:13 +0530

    typeb_with_flag_done

--- simulator/type_b_func.py
+++ simulator/type_b_func.py
@@ -2,13 +2,19 @@ register_list = ['0000000000000000','0000000000001000','0000000000000011','00000
 
 
 def movi(machine_ins):
+    global PC
     global register_list
     reg = int(machine_ins[5:8],2)
     imm = machine_ins[8:]
     register_list[reg] = "00000000"+imm
 
+    register_list[6] = '0000000000000000'
+    PC+=1
+
 
 def rs(machine_ins):
+    global PC
+    global register_list
     reg = int(machine_ins[5:8],2)
     imm = int(machine_ins[8:],2)
     if (imm > 16):
@@ -16,9 +22,14 @@ def rs(machine_ins):
     else:
         register_list[reg] = register_list[reg][imm:]
         register_list[reg] = register_list[reg] + imm*"0"
+    
+    register_list[6] = '0000000000000000'
+    PC+=1
         
 
 def ls(machine_ins):
+    global PC
+    global register_list
     reg = int(machine_ins[5:8],2)
     imm = int(machine_ins[8:],2)
     if (imm > 16):
@@ -26,6 +37,9 @@ def ls(machine_ins):
     else:
         register_list[reg] = register_list[reg][0:(16-imm)]
         register_list[reg] = imm*"0" + register_list[reg]
+    
+    register_list[6] = '0000000000000000'
+    PC+=1
         
 
 #movi("1000000001010011")
