d963ade68445471d10b5361fffc2e8dcef6b6819
zubaida03 <zubaidafh13@hotmail.com>
2022-08-06 21:03:37 +0530

    menu added

--- simulator/question5.py
+++ simulator/question5.py
@@ -8,6 +8,7 @@ addressable_types = {"BIT ADDRESSABLE MEMORY":1/8,
 units = {"B":0,"KB":10,"MB":20,"GB":30,"TB":40, "WORD": 0, "KWORD": 10, "MWORD": 20, "GWORD":30, "TWORD":40}
 
 def isaq(mem_space, addressable_types, mem_add_type, unit, mem_space_value):
+    global address_len
     instr_len = int(input("Enter length of instruction in bits: "))
     reg_len = int(input("Enter length of register in bits: "))
     if len(unit)>=2:
@@ -24,13 +25,15 @@ def isaq(mem_space, addressable_types, mem_add_type, unit, mem_space_value):
     maxnum_reg = 2**reg_len
     if int(address_len)>int(instr_len):
         print("Error: Instruction length is too small")
+    elif int(filler_bits)<0:
+        print('Error: Filler bits are negative')
     else:
         print('Minimum bits to represent an address: ',int(address_len))
         print('Number of bits needed by opcode: ',int(opcode))
         print('Number of filler bits: ',int(filler_bits))
         print('Number of maximum instructions: ',int(maxnum_instr))
         print('Maximum number of registers: ',maxnum_reg)
-    return address_len
+
 
 
 def type1(addressable_types, mem_space_value, old_add_len, unit, units):
@@ -39,8 +42,20 @@ def type1(addressable_types, mem_space_value, old_add_len, unit, units):
     for keys in addressable_types.keys():
         i+=1
         print(i,":",keys)
-    new_mem_add_type=input('Input change in how you would like to address memory: ')
-    new_mem_add_type=new_mem_add_type.upper()
+    
+
+    #new_mem_add_type=input('Input change in how you would like to address memory: ')
+    #new_mem_add_type=new_mem_add_type.upper()
+    op=int(input("Input option for how memory has to be addressed: "))
+    if op==1:
+        new_mem_add_type='BIT ADDRESSABLE MEMORY'
+    elif op==2:
+        new_mem_add_type='NIBBLE ADDRESSABLE MEMORY'
+    elif op==4:
+        new_mem_add_type='WORD ADDRESSABLE MEMORY'
+    else:
+        new_mem_add_type='BYTE ADDRESSABLE MEMORY'
+    
     if len(unit)>=2:
         ans = units[unit.upper()]  
     else:
@@ -65,8 +80,21 @@ def type1(addressable_types, mem_space_value, old_add_len, unit, units):
 
 def type2(addressable_types, mem_space_value,unit, units):
     cpu=int(input("Input the number of bits of CPU: "))
+    v=int(math.log(cpu,2))
     add_pin=int(input("Input address pins: "))
-    mem_type=input("Input the type of memory: ")
+    i=0
+    for keys in addressable_types.keys():
+        i+=1
+        print(i,":",keys)
+    op=int(input("Input option for how memory has to be addressed: "))
+    if op==1:
+        mem_type='BIT ADDRESSABLE MEMORY'
+    elif op==2:
+        mem_type='NIBBLE ADDRESSABLE MEMORY'
+    elif op==4:
+        mem_type='WORD ADDRESSABLE MEMORY'
+    else:
+        mem_type='BYTE ADDRESSABLE MEMORY'
 
     if mem_type.upper()=='BIT ADDRESSABLE MEMORY':
         mem_val=add_pin-3
@@ -75,7 +103,7 @@ def type2(addressable_types, mem_space_value,unit, units):
     elif mem_type.upper()=='BYTE ADDRESSABLE MEMORY':
         mem_val=add_pin
     else:
-        mem_val=add_pin+3
+        mem_val=add_pin+v
 
     if mem_val<10:
         print('Size of main memory: ',2**mem_val, ' B')
@@ -114,14 +142,40 @@ i=0
 for keys in addressable_types.keys():
     i+=1
     print(i,":",keys)
-mem_add_type = str(input("Enter how you would like to address memory: "))
+#mem_add_type = str(input("Enter how you would like to address memory: "))
+
+op=int(input("Input option for how memory has to be addressed: "))
+if op==1:
+    mem_add_type='BIT ADDRESSABLE MEMORY'
+elif op==2:
+    mem_add_type='NIBBLE ADDRESSABLE MEMORY'
+elif op==4:
+    mem_add_type='WORD ADDRESSABLE MEMORY'
+else:
+    mem_add_type='BYTE ADDRESSABLE MEMORY'
 # mem_add_type = "BYTE ADDRESSABLE MEMORY"
 
 mem_add_type=mem_add_type.upper()
 #address_len=isaq(mem_space, addressable_types, mem_add_type, unit,nmem_space_value)
-address_len=isaq(mem_space, addressable_types, mem_add_type, unit,mem_space_value)
 
+while True:
+    print("Select one of the following options:")
+    print('1. ISA and instructions related')
+    print('2. System enhancement related')
+    print('3. Exit')
+    j=int(input("Option[1-2]: "))
+    if j==1:
+        isaq(mem_space, addressable_types, mem_add_type, unit,mem_space_value)
+    if j==2:
+        print("Select one of the types in system enchancement:")
+        print('1. Type 1')
+        print('2. Type 2')
+        k=int(input("Option[1-2]: "))
+        if k==1:
+            type1(addressable_types, mem_space_value, address_len, unit, units)
 
+        else:
 
-type1(addressable_types, mem_space_value, address_len, unit, units)
-type2(addressable_types, mem_space_value,unit, units)
+            type2(addressable_types, mem_space_value,unit, units)
+    if j==3:
+        break
